3bc0558995a477745e746398c284e7ed6c10f023
Adam <66421205+adamj06@users.noreply.github.com>
2025-01-05 22:21:14 +0000

    Add Weather to app and fix API issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react'
+
 import Search from './components/Search'
+import WeatherDisplay from './components/WeatherDisplay'
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null)
+
   const handleSearch = (search) => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${import.meta.env.VITE_OPENWEATHERMAP_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${import.meta.env.VITE_OPENWEATHERMAP_API_KEY}`
     )
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) {
+      throw new Error('Network response was not ok')
+      }
+
+      return res.json()
+    })
     .then((data) => {
-      console.log(data)
+      setWeatherData(data)
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error)
     })
   }
 
   return (
     <>
       <Search onSearch={handleSearch} />
+      <WeatherDisplay data={weatherData} />
     </>
   )
 }
