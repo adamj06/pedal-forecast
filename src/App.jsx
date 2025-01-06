033384bc57dbbb6731f9f8785f4b0db82237aab6
Adam <66421205+adamj06@users.noreply.github.com>
2025-01-06 15:34:31 +0000

    Change WeatherDisplay styling

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ function App() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
+
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
@@ -70,8 +71,11 @@ function App() {
               See the weather forecast for your next bike ride.
             </p>
             <Search onSearch={fetchWeather} />
+            <div className="mt-8"></div>
+            <WeatherDisplay data={weatherData} />
           </div>
         </div>
+
         <div
           aria-hidden="true"
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
